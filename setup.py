568e9ffccfe08add00bda44d7766bad2129effce
PRATEEK MAHESHWARI <Prateek.Maheshwari@in.bosch.com>
2022-03-23 13:25:02 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Short project to undertand the basics of MLOPS',
+    author='Prateek_Maheshwari',
+    license='MIT',
+)
